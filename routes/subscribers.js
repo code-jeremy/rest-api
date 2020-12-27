04f475c671a85d76e501ebf6f894dd18c3e17139
Jeremy Walters <Jeremy@jeremys-air.myfiosgateway.com>
2020-12-27 13:47:27 -0500

    Configs 1

---
+++ routes/subscribers.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+//Getting all 
+router.get('/', (req, res) => {
+
+})
+//Getting One
+router.get('/:id', (req, res) => {
+    
+})
+//Creating One
+router.post('/', (req, res) => {
+
+})
+//Updating One
+router.patch('/:id', (req, res) => {
+
+})
+//Deleting One
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+module.exports = router
